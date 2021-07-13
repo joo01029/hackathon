124726087ee0ba171f44af8e0faeb30f078e518c
joo01029 <62934181+joo01029@users.noreply.github.com>
2021-07-13 15:55:41 +0900

    [update] remove HEAD method

--- src/main/java/com/hackathon/filter/CorsFilter.java
+++ src/main/java/com/hackathon/filter/CorsFilter.java
@@ -19,7 +19,7 @@ public class CorsFilter implements Filter {
 
 		res.setHeader("Access-Control-Allow-Origin", "*");
 		res.setHeader("Access-Control-Allow-Credentials", "true");
-		res.setHeader("Access-Control-Allow-Methods", "POST,GET,PUT,OPTIONS,DELETE,PATCH,HEAD");
+		res.setHeader("Access-Control-Allow-Methods", "POST,GET,PUT,OPTIONS,DELETE,PATCH");
 		res.setHeader("Access-Control-Max-Age", "3600");
 		res.setHeader("Access-Control-Allow-Headers", "X-Request-With, Content-Type, ContentType, Authorization, Origin, Accept, Access-Control-Request-Method, Access-Control-Request-header, Cache-Control, Pragma, Expires, Media-Type");
 		res.setHeader("Access-Control-Expose-Headers", "content-disposition");
